59e44c9cd7f676e5309f171206295eea286dc330
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-01-26 21:25:57 +0900

    feat: add prompt engineering experiment

---
+++ 4-prompt-engineering/zero_shot_cot/zero_shot_cot.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from chatgpt4pcg.competition import chat_with_chatgpt, run_evaluation
+from chatgpt4pcg.models.trial_context import TrialContext
+from chatgpt4pcg.models.trial_loop import TrialLoop
+from dotenv import load_dotenv
+
+
+class ZeroShotChainOfThoughtPrompting(TrialLoop):
+    @staticmethod
+    def run(ctx: TrialContext, target_character: str) -> str:
+        """
+        Runs the zero-shot chain-of-thought prompting.
+        :param ctx: The trial context.
+        :param target_character: The target character.
+        :return: The generated text.
+        """
+        prompt_template = open(Path("prompts/zero_shot_cot.txt"), "r").read()
+        response = chat_with_chatgpt(ctx, [{"role": "user", "content": prompt_template
+                                     .replace("<OBJECT>", target_character)}])
+        response = response[0]
+        return response
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    run_evaluation("zero_shot_cot", ZeroShotChainOfThoughtPrompting)
